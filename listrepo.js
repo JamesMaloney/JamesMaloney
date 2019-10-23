3d2593ef24a80b749b903c77ae456f80429dcb66
JamesMaloney <giacomo.menchi@gmail.com>
2019-10-23 17:50:27 +0200

    Ok, let's rewind it back

--- listrepo.js
+++ listrepo.js
@@ -1,8 +1,7 @@
 //iOS Device Checker
-var divertimento = "Add this repo to Cydia";
 is_ios = (navigator.userAgent.match(/iPad/i) != null) || (navigator.userAgent.match(/iPhone/i) != null) || (navigator.userAgent.match(/iPod/i) != null);
 if(is_ios)
-	document.getElementById('idevice').innerHTML = divertimento;
+	document.getElementById('idevice').innerHTML = 'Add this repo to Cydia';
 else {
 	document.getElementById('idevice').style.color = "red";
 	document.getElementById('idevice').innerHTML = 'Only available through iDevice!';
