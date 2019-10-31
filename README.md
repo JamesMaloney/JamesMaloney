35478e9fcc576bfdf34c69223f801ae693e3b593
JamesMaloney <giacomo.menchi@gmail.com>
2019-10-31 14:41:57 +0100

    New folders, no more old stuff, and a little bit of parameters

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-Welcome to my own Cydia Repo, where I repost other's packages that I like (and sometimes even publish some small "creation")
+Welcome to my own Cydia Repo, where I repost other's packages that I like (and sometimes even publish some small "creation").
 
 TODO:
 
-- Remove old useless stuff
 - Automatize pages creation
 - Create custom depictions
--- tweaklist.js
+++ tweaklist.js
@@ -75,7 +75,7 @@ xhr.onreadystatechange = function() {
 		var pack = packagesList[key];
 		//The following code is very messy. You have been warned.
 		document.getElementById('tweaks').innerHTML = document.getElementById('tweaks').innerHTML
-		+ '<a href="pages/' + pack.Package + '">'
+		+ '<a href="pages/' + pack.Package + '/index.html?id=com.example.identifier">'
 		+ '<img class="icon" src="icons/' + pack.Package + '.png" onerror="this.src=\'icons/default.png\';" width="58" height="58">'
 		+ '<div><label>' + pack.Name + '</label></div></a>';
 	}
