3c747519472aa38008019b2a4dbe9c5121bf1952
JamesMaloney <giacomo.menchi@gmail.com>
2022-06-30 16:10:21 +0200

    First dynamic model name attempt

--- artest/selector.js
+++ artest/selector.js
@@ -2,4 +2,5 @@
 var url = new URL(window.location.href);
 var shoe_name = url.searchParams.get("name");
 
-console.log(shoe_name)
+document.getElementById("model").setAttribute("src", shoe_name + ".glb");
+document.getElementById("model").setAttribute("ios-src", shoe_name + ".usdz");
